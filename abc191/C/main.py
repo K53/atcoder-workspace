a4515ba26ab864699eb7bc815c440e9e6e7d7d61
keigo <devend.keigo@gmail.com>
2021-02-07 02:03:43 +0900

    abc191

---
+++ abc191/C/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+def main():
+    ans = 0
+    H, W = map(int, input().split())
+    l = []
+    for _ in range(H):
+        l.append(input())
+    
+    for h in range(H - 1):
+        for w in range(W - 1):
+            c = 0
+            if l[h][w] == "#":
+                c += 1
+            if l[h][w + 1] == "#":
+                c += 1
+            if l[h + 1][w] == "#":
+                c += 1
+            if l[h + 1][w + 1] == "#":
+                c += 1
+            if c == 1 or c == 3:
+                ans += 1
+    print
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
