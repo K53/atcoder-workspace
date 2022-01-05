e4559603e65f2d0c1d26aaa89bb4780d0d097d30
keigo <devenv.keigo@gmail.com>
2022-01-05 22:54:44 +0900

    generalize seg tree lib

--- abc185/F/main.py
+++ abc185/F/main.py
@@ -2,52 +2,72 @@
 import sys
 
 class SegmentTree:
-    def __init__(self, initVal: int, bottomLen: int, func: "function(int, int)"):
-        self.initVal = initVal
-        self.func = func
+    def __init__(self, monoid: int, bottomLen: int, operation: "function"):
+        self.monoid = monoid
         self.bottomLen = bottomLen
         self.offset = self.bottomLen        # セグ木の最下層の最初のインデックスに合わせるためのオフセット
         self.segLen = self.bottomLen * 2
-        self.tree = [initVal] * self.segLen
+        self.tree = [monoid] * self.segLen
+        self.operation = operation
+        return
+    
+    """ 1点更新
+    O(1)
+    """
+    def pointUpdateWithoutRebuild(self, index: int, val: int):
+        segIndex = index + self.offset
+        self.tree[segIndex] ^= val          # 各マスの更新方法
+        return
+    
+    """ 全区間更新
+    O(bottomLen) # =セグ木の配列長
+    """
+    def allBuild(self):
+        for segIndex in reversed(range(self.offset)):
+            if segIndex == 0:
+                return
+            self.tree[segIndex] = self.operation(self.tree[segIndex * 2], self.tree[segIndex * 2 + 1])
+        return
 
-    """ 一点更新
-    tree[index] = val
+    """ 1点更新 + リビルド
+    O(log(bottomLen))
     """
     def pointUpdate(self, index: int, val: int):
         segIndex = index + self.offset
-        self.tree[segIndex] = self.func(self.tree[segIndex], val) # Update
+        self.tree[segIndex] ^= val          # 各マスの更新方法
         while True:
             segIndex //= 2
             if segIndex == 0:
                 break
-            self.tree[segIndex] = self.func(self.tree[segIndex * 2], self.tree[segIndex * 2 + 1])
+            self.tree[segIndex] = self.operation(self.tree[segIndex * 2], self.tree[segIndex * 2 + 1])
+        return
 
-    """ 区間最小値 (RMQ)
-    """
     def rangeQuery(self, l: int, r: int):
         l += self.offset
         r += self.offset
-        res = self.initVal
+        res = self.monoid                   # クエリの初期値
         while l < r:
             if l % 2 == 1:
-                res = self.func(res, self.tree[l])
+                res = self.operation(res, self.tree[l])
                 l += 1
             l //= 2
             if r % 2 == 1:
-                res = self.func(res, self.tree[r - 1])
+                res = self.operation(res, self.tree[r - 1])
                 r -= 1
             r //= 2
         return res
 
-def myXor(a, b):
+
+def Xor(a, b):
     return a ^ b
 
 def main():
     N, Q = map(int, input().split())
     A = list(map(int, input().split()))
-    tr = SegmentTree(initVal=0, bottomLen=(N), func=myXor)
+    tr = SegmentTree(monoid=0, bottomLen=(N), operation=Xor)
     for nn in range(N):
-        tr.pointUpdate(index=nn, val=A[nn])
+        tr.pointUpdateWithoutRebuild(index=nn, val=A[nn])
+    tr.allBuild()
     for _ in range(Q):
         T, X, Y = map(int, input().split())
         if T == 1:
